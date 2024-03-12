ab00b5ae7407c32b215a74ace5657b2e34f83acb
hamsih221 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2024-03-12 17:02:06 +0100

    Refactor code to retrieve star information from Simbad

--- stars_from_simbad.py
+++ stars_from_simbad.py
@@ -1,11 +1,20 @@
 from astroquery.simbad import Simbad
-
-parsec_distance = 15.33
-
+# test star "* tet Per"
+parsec_distance = 15.33  # 15.33
 result_table = Simbad.query_criteria(
-    "Distance.unit = 'pc'", f"Distance.distance < {parsec_distance}")
+    "Distance.unit = 'pc'",
+    f"Distance.distance < {parsec_distance}")
+
 
 for row in result_table:
-    print(row["MAIN_ID"])
-
+    try:
+        Simbad.add_votable_fields("distance", "diameter")
+        object = Simbad.query_object(row["MAIN_ID"])
+        if object["Diameter_unit"] != "mas":
+            continue
+        print(object["MAIN_ID"], object["Distance_distance"],
+              object["Distance_unit"], object["Diameter_diameter"], object["Diameter_unit"], sep="\n")
+    except Exception as e:
+        print(row["MAIN_ID"], row["RA"], row["DEC"], "No measurements")
+        print(e)
 print(len(result_table))
