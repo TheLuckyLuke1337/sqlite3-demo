013e76f3db04c2f5372e5e577a3723bb35a75fe8
hamsih221 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2024-03-12 16:20:05 +0100

    Update distance filter in stars_from_simbad.py

--- stars_from_simbad.py
+++ stars_from_simbad.py
@@ -1,8 +1,9 @@
 from astroquery.simbad import Simbad
 
+parsec_distance = 15.33
 
 result_table = Simbad.query_criteria(
-    "Distance.unit = 'pc'", "Distance.distance < 15.33")
+    "Distance.unit = 'pc'", f"Distance.distance < {parsec_distance}")
 
 for row in result_table:
     print(row["MAIN_ID"])
