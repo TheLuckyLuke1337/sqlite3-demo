2da275ba3ce26de84084b0e14c2bca4b58270c65
hamsih221 <112425988+TheLuckyLuke1337@users.noreply.github.com>
2024-03-18 13:24:52 +0100

    Add astropy.table import and modify JOIN statements in query

--- test2.py
+++ test2.py
@@ -1,5 +1,6 @@
 import pyvo
 from cartesian import polar2cartesian
+from astropy.table import Table
 
 tap_service_url = "http://simbad.cds.unistra.fr/simbad/sim-tap"
 
@@ -12,13 +13,13 @@ AVG(diameter) AS avg_diameter,
 AVG(dist) AS avg_distance, ra, dec,
 sp_type, main_id, AVG(allfluxes.v) as avg_v
 FROM basic
-JOIN mesDiameter ON basic.oid = mesDiameter.oidref
 JOIN mesDistance ON basic.oid = mesDistance.oidref
-JOIN mesSpT ON basic.oid = mesSpT.oidref
-JOIN mesFe_H ON basic.oid = mesFe_H.oidref
+LEFT JOIN mesDiameter ON basic.oid = mesDiameter.oidref AND mesDiameter.unit = 'km'
+LEFT JOIN mesSpT ON basic.oid = mesSpT.oidref
+LEFT JOIN mesFe_H ON basic.oid = mesFe_H.oidref
 JOIN allfluxes ON basic.oid = allfluxes.oidref
-WHERE mesDiameter.unit = 'km'
 AND mesDistance.unit = 'pc'
+AND mesDistance.dist < 15.33
 GROUP BY basic.oid;
 """
 
@@ -28,7 +29,11 @@ table = tap_service.run_async(query).to_table()
 
 table["x"], table["y"], table["z"] = polar2cartesian(
     table["ra"], table["dec"], table["avg_distance"])
+result = Table()
 
-print(table["x", "y", "z", "avg_v", "avg_temperature",
-            "avg_diameter", "sp_type", "main_id",
-            ])
+result_keys = ["x", "y", "z", "flux_v", "temperature", "sp_type", "main_id"]
+table_keys = ["x", "y", "z", "avg_v", "avg_temperature", "sp_type", "main_id"]
+for result_key, table_key in zip(result_keys, table_keys):
+    result[result_key] = table[table_key]
+result["radius_km"] = table["avg_diameter"] / 2
+print(result.colnames)
